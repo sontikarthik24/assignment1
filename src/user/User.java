68f86c1fcb9bc9b3172ae29bc820b84669b54571
Karthik Sonti <karthiksonti@Karthiks-MacBook-Pro.local>
2022-09-19 08:17:19 -0400

    initial commit

---
+++ src/user/User.java
@@ -0,0 +1,199 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package user;
+
+import java.util.Date;
+
+/**
+ *
+ * @author karthiksonti
+ */
+public class User {
+    
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private int age;
+    private String phoneNumber;
+    private String emailAddress;
+    private String streetLine1;
+    private String streetLine2;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String degree1;
+    private String degree1StartDate;
+    private String degree1EndDate;
+    private String degree2;
+    private String degree2StartDate;
+    private String degree2EndDate;
+    private String photo;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getStreetLine1() {
+        return streetLine1;
+    }
+
+    public void setStreetLine1(String streetLine1) {
+        this.streetLine1 = streetLine1;
+    }
+
+    public String getStreetLine2() {
+        return streetLine2;
+    }
+
+    public void setStreetLine2(String streetLine2) {
+        this.streetLine2 = streetLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getDegree1() {
+        return degree1;
+    }
+
+    public void setDegree1(String degree1) {
+        this.degree1 = degree1;
+    }
+
+    public String getDegree1StartDate() {
+        return degree1StartDate;
+    }
+
+    public void setDegree1StartDate(String degree1StartDate) {
+        this.degree1StartDate = degree1StartDate;
+    }
+
+    public String getDegree1EndDate() {
+        return degree1EndDate;
+    }
+
+    public void setDegree1EndDate(String degree1EndDate) {
+        this.degree1EndDate = degree1EndDate;
+    }
+
+    public String getDegree2() {
+        return degree2;
+    }
+
+    public void setDegree2(String degree2) {
+        this.degree2 = degree2;
+    }
+
+    public String getDegree2StartDate() {
+        return degree2StartDate;
+    }
+
+    public void setDegree2StartDate(String degree2StartDate) {
+        this.degree2StartDate = degree2StartDate;
+    }
+
+    public String getDegree2EndDate() {
+        return degree2EndDate;
+    }
+
+    public void setDegree2EndDate(String degree2EndDate) {
+        this.degree2EndDate = degree2EndDate;
+    }
+    
+    
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    
+    
+
+}
